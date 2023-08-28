095bd827f6838675ae0e4cce1e69d5b5eae05370
romildoran <ronald260986@gmail.com>
2023-08-27 20:31:47 -0500

    creacion del slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200,
+        responsive: true,
+        pager: true,
+        auto: true,
+    });
+});
